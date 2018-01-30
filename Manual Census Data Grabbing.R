8ade0b84a5adcb6110c6d3400d07ac5fd4933247
Chris Kiniry <chriskiniry@gmail.com>
2018-01-29 20:53:23 -0600

    Updated script to run on different geography I had downloaded data for.

--- Manual Census Data Grabbing.R
+++ Manual Census Data Grabbing.R
@@ -41,8 +41,45 @@ tmap_mode("view")
 my_map
 
 
+rm(list=ls())
 
 
+
+
+# Part 2 ------------------------------------------------------------------
+
+
+# Look in dir with Shapefiles downloaded from Census
+dir("Shapefiles 2")
+
+# Read in the shapefiles, specify a directory and filename (not extensions)
+collin <- readOGR(dsn = "Shapefiles 2", layer = "14000")
+
+# Get a summary of the shape data
+summary(collin)
+
+
+# Read in the downloaded Census Data
+dir("Income Data 2")
+data <- read_csv("Income Data 2/ACS_16_5YR_B19013_with_ann.csv")
+
+# Delete second header row, and convert to numeric data type
+data <- data[-1,]
+data <- data %>% 
+  mutate(HD01_VD01 = as.numeric(HD01_VD01))
+
+# Join the data to the spatial data
+spat_data <- merge(collin, data, by.x = "GEO_ID", by.y = "GEO.id" )
+
+
+# Making the maps
+tm_shape(spat_data) + tm_fill(col = "HD01_VD01")
+
+my_map <- tm_shape(spat_data) + tm_fill(col = "HD01_VD01")
+
+tmap_mode("view")
+
+my_map
 
 
 
